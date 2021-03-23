183e92ab7ee4258bcab29b60cc1fbf65f5042c15
Seb <seb@gavisus.net>
2021-03-23 20:49:23 +1100

    Trying to fix CORS issue

--- server/src/index.js
+++ server/src/index.js
@@ -14,9 +14,9 @@ const pool = new Pool({
 })
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://tasks.sebhavens.com');
     res.header('Access-Control-Allow-Headers', '*');
-    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE');
     next();
 });
 
